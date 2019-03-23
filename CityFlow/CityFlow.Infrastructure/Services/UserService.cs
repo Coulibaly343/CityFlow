ad552d3dd4cb2ae3e20a282fa5db8eda1c25d4aa
sm00thy <ibalance5@gmail.com>
2019-03-23 03:20:49 +0100

    user serv

--- CityFlow/CityFlow.Infrastructure/Services/UserService.cs
+++ CityFlow/CityFlow.Infrastructure/Services/UserService.cs
@@ -1,19 +1,27 @@
 using System.Threading.Tasks;
 using CityFlow.Core.Entity;
+using CityFlow.Infrastructure.Repositories.Interfaces;
 using CityFlow.Infrastructure.Services.Interfaces;
 
 namespace CityFlow.Infrastructure.Services
 {
     public class UserService : IUserService
     {
+        private readonly IRepository<User> _userRepo;
+
+        public UserService(IRepository<User> userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        
         public Task<int> AddUserAsync(string name)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<User> GetUserByIdAsync(int userId)
+        public async Task<User> GetUserByIdAsync(int userId)
         {
-            throw new System.NotImplementedException();
+            return await _userRepo.GetByIdAsync(userId);
         }
     }
 }
